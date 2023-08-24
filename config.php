d4ac5e3f9b1581f3060be14645400a33d223fd1f
jeevarani208 <142564903+jeevarani208@users.noreply.github.com>
2023-08-24 15:04:14 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+$conn = mysqli_connect('localhost','root','','user_db') or die('connection failed');
+
+
+?>
+
+<?php 
+/*
+<?php
+$host = "sql101.unaux.com";
+$user = "unaux_34858025";
+$pass = "lr9sgl";
+$db = "unaux_34858025_user_db";
+
+$conn = mysqli_connect($host,$user,$pass,$db) or die('connection failed');
+
+?>
+
+*/
+ ?>
+
